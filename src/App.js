1e2808e2239d6c2ac11cc7c0ef57bc819799fcd7
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-16 15:15:32 +0100

    App.js: added navigation between old and new looks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import './global.css';
+import './newComponent/css/bootstrap.min.css';
+import './newComponent/fontawesome/css/all.min.css';
+import './newComponent/css/templatemo-diagoona.css';
+import './newComponent/css/motion.css';
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
@@ -11,20 +15,28 @@ import DafetiteSchema from './schemas/dafetiteSchema';
 // import { AlertProvider } from "./context/alertContext";
 // import Alert from "./components/Alert";
 import { useSetHeadTag } from './hooks/useSetHeadTag'
+import AppRoutes from './newComponent/routes/Route';
+import { useLocation } from 'react-router-dom';
 
 function App() {
   useSetHeadTag()
+  const path = useLocation().pathname;
+  console.log("Current path:", path); // Debugging line to check the current path
+  const isMainSite = path === '/';
   return (
-    <ChakraProvider>
-      <DafetiteSchema />
-      <main>
-        <Header />
-        <LandingSection />
-        <ProjectsSection />
-        {/* <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ogagadafetite" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ng.linkedin.com/in/ogagadafetite?trk=profile-badge">Dafetite Ogaga</a></div> */}
-        <Footer />
-      </main>
-    </ChakraProvider>
+    <>
+      <AppRoutes />
+      {isMainSite && <ChakraProvider>
+        <DafetiteSchema />
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectsSection />
+          {/* <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ogagadafetite" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ng.linkedin.com/in/ogagadafetite?trk=profile-badge">Dafetite Ogaga</a></div> */}
+          <Footer />
+        </main>
+      </ChakraProvider>}
+    </>
   );
 }
 
