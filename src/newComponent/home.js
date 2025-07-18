c5ffa77ff7ef728653fc4f1f249361a5d9eed23f
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 14:48:09 +0100

    home.js: added data from entry

--- src/newComponent/home.js
+++ src/newComponent/home.js
@@ -10,18 +10,19 @@ import {
 } from '../entry/Entry';
 
 function Home() {
+	console.log("Home component loaded");
 	const [showMore, setShowMore] = useState(false);
 	return (
-		<main className="tm-col-right fade-in-from-bottom">
+		<main className="tm-col-right">
 			<div style={{display: 'flex', flexDirection: 'column'}}>
 				<section className="tm-content">
-				<h2 className="tm-content-title"><strong>Professional Summary</strong></h2>
+				<h2 className="tm-content-title fade-in-from-bottom"><strong>Professional Summary</strong></h2>
 					{/* Summary */}
 					{professionalSummary.map((item, index) => {
 						const last = professionalSummary.length - 1 === index;
 						return (
 							<Fragment key={index}>
-								<p className="mb-5" style={{lineHeight: 1.5}}>{item.text}</p>
+								<p className="mb-5 fade-in-from-bottom" style={{lineHeight: 1.5}}>{item.text}</p>
 								{!last && <hr className="mb-5"/>}
 								{/* <p className="mb-5">Diagoona is Bootstrap v4.4.1 layout. This BG is 50% transparent. You can set the background images auto play settings (true or false in line number 33) in templatemo-script.js file in js folder.</p> */}
 							</Fragment>
@@ -87,7 +88,7 @@ function Home() {
 								<h4 className="underlined"><strong>{item.certifications}</strong></h4>
 								{item.certificates.map((certificate, certificateIndex) => {
 									return (
-										<div key={`${index}${certificateIndex}`} className="mb-5">
+										<div key={`${index}${certificateIndex}`} className="mb-5 link-hover">
 											<Link to={certificate.url} className="underlined" style={{color: 'white'}}><h5 style={{margin: 0}}>{certificate.name}</h5></Link>
 											{/* <p  className="italiced" style={{marginBottom: 0}}><strong>{project.company}</strong> - {project.duration}</p> */}
 											<p style={{marginBottom: -15}}>Certified by: <strong>{certificate.provider}</strong></p>
@@ -143,7 +144,7 @@ function Home() {
 				<div>
 					<span style={{borderRadius: 5}}
 					onClick={()=> setShowMore(prev=>!prev)}
-					className="btn btn-primary">{showMore?'Collapse':'Read More...'}</span>
+					className="btn btn-primary right-fade-in">{showMore?'Collapse':'Read More...'}</span>
 				</div>
 			</div>
 		</main>
