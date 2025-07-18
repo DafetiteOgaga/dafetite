21daa64c641eb990f8872ce3a62175ed6c64a041
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 15:00:15 +0100

    project.js: added projects

--- src/newComponent/project.js
+++ src/newComponent/project.js
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
+import { projectCardsContent } from '../entry/Entry';
 
 function Project () {
 	console.log("Project component loaded");
 	return (
 		<>
-			<main className="tm-col-right fade-in-from-bottom">
-				<section className="tm-content tm-about">
-					<h2 className="mb-5 tm-content-title">About Diagoona Template</h2>
-					<hr className="mb-4"/>
-					<div className="media my-3">
-						<i className="fas fa-shapes fa-3x p-3 mr-4"></i>
-						<div className="media-body">
-							<p>Cras quam urna, interdum at posuere ac, tincidunt ut ipsum. Nam condimentum placerat enim. Nullam euismod sapien.</p>
-						</div>
-					</div>
-					<div className="media my-3">
-						<i className="fas fa-draw-polygon fa-3x p-3 mr-4"></i>
-						<div className="media-body">
-							<p>Nunc id hendrerit nunc. Etiam ultricies arcu sem, vel dapibus lacus lacinia quis. Nunc auctor placerat nisi ac ultrices.</p>
-						</div>
-					</div>
-					<div className="media my-3">
-						<i className="fab fa-creative-commons-share fa-3x p-3 mr-4"></i>
-						<div className="media-body">
-							<p>Vestibulum imperdiet hendrerit nibh. Integer sit amet lacus et nunc auctor tincidunt eu ac sapien. Ut suscipit velit eget faucibus finibus.</p>
-						</div>
-					</div>
-					<div className="media my-3">
-						<i className="fas fa-bookmark fa-3x p-3 mr-4"></i>
-						<div className="media-body">
-							<p>Prasent eget enim vitae sapien egestas aliquet non quis neque. Duis pharetra varius massa, ut bibendum tortor sodales ac.</p>
-						</div>
-					</div>
+			<main className="tm-col-right">
+				<section id='section-projects-id' className="tm-content">
+					{projectCardsContent.map((item, index) => {
+						const animationDelay = '1s'
+						return (
+							<Fragment key={index}>
+								<h2 style={{animationDelay: animationDelay}}
+								className="tm-project-content-title underlined italiced">{item.status}</h2>
+								{item.content.map((content, contentIndex) => {
+									return (
+											<Fragment key={`${index}${contentIndex}`}>
+												<Link to={content.url}
+												target="_blank"
+												rel="noopener noreferrer"
+												style={{color: 'inherit', animationDelay: animationDelay}}
+												className="media my-3 mb-5 tm-service-media tm-service-media-img-l link-hover">
+													{contentIndex%2 ?
+													<>
+														<div className="media-body tm-service-text">
+															<h2 style={{marginTop: -7}} className="mb-0 tm-project-content-title"><strong>{content.title}</strong></h2>
+															<p style={{lineHeight: 1.4}}>{content.description}</p>
+														</div>
+														<img src={content.getImageSrc} alt={content.getImageSrc} className={"tm-service-img-r"}/>
+													</>
+													:
+													<>
+														<img src={content.getImageSrc} alt={content.getImageSrc} className={"tm-service-img"}/>
+														<div className="media-body tm-service-text">
+															<h2 style={{marginTop: -7}} className="mb-0 tm-project-content-title"><strong>{content.title}</strong></h2>
+															<p style={{lineHeight: 1.4}}>{content.description}</p>
+														</div>
+													</>}
+												</Link>
+											</Fragment>
+									)
+								})}
+							</Fragment>
+						)
+					})}
 				</section>
 			</main>
 		</>
