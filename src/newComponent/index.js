eaf9f470b47d99fd433989854cb8adf7fbeb8e3a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 01:04:49 +0100

    index.js: fixed up component for dynamic use

--- src/newComponent/index.js
+++ src/newComponent/index.js
@@ -1,90 +1,69 @@
 import React from 'react';
-// import './css/bootstrap.min.css';
-// import './fontawesome/css/all.min.css';
-// import './css/templatemo-diagoona.css';
-import { Link } from 'react-router-dom'
+// import '../App.css';
+// import '../global.css';
+import { Outlet } from 'react-router-dom';
 import { BackgroundSlideshow, ManualBackgroundSelector, BackgroundSlideshowProvider } from './hooks/backgroundSlideshow';
-// import testimage from './img/diagoona-bg-1.jpg';
+import { SiteIconAndSocials } from './siteIconAndSocials';
+import { NavigationBar } from './navigationBar';
+import { Footer } from './footer';
+import dafetite from '../dafetite.png';
 
+const skills = [
+	'Software and Automation Engineer', 'Fullstack (Web and Mobile) Developer', 'IT Support',
+	'Automated Software Tester', 'Programming Tutor', 'ATM and Technical Engineer'
+]
 function Index () {
 	console.log("Index component loaded");
-	// console.log('BackgroundSlideshow is:', BackgroundSlideshow);
 	return (
 		<>
 			<BackgroundSlideshowProvider>
 				<BackgroundSlideshow />
 				<div className="tm-container">
 					<div>
+						<div
+						// className='tm-row-baseline-align'
+						>
 						<div className="tm-row pt-4">
+							<SiteIconAndSocials />
+							<NavigationBar />
+						</div>
+						</div>
+						<div className="tm-row">
+							{/* left box */}
 							<div className="tm-col-left">
-								<div className="tm-site-header media left-slide-in">
-									<i className="fas fa-umbrella-beach fa-3x mt-1 tm-logo"></i>
-									<div className="media-body">
-										<h1 className="tm-sitename text-uppercase">diagoona</h1>
-										<p className="tm-slogon">new bootstrap template</p>
-									</div>        
+								<div className='profile-container'>
+									<img src={dafetite} alt="Dafetite Ogaga" className="dafetite-profile image-fade fade-in-from-top" />
+									<div className="tm-col-left-content">
+										<h1 style={{animationDelay: '0.s', margin: 0, fontSize: 36}}
+										className="tm-sitename text-uppercase flip-and-fade-in">Dafetite O. Ogaga</h1>
+										<p className='italiced underlined' style={{marginTop: -10}}>Email: <strong>ogagadafetite@gmail.com</strong></p>
+										{skills.map((skill, index) => {
+											const animetionDelay = `${0.4 + index * 0.4}s`;
+											return (
+												<p key={index}
+												className="tm-slogon skill-fade-in-from-top" style={{
+													animationDelay: animetionDelay,
+													lineHeight: '0.3rem',
+												}}>|| {skill}</p>
+											)
+										})}
+										{/* <p className="tm-slogon">a personal portfolio</p>
+										<p className="tm-slogon">a ReactJS project</p>
+										<p className="tm-slogon">a template by <a rel="nofollow" target="_parent" href="https://templatemo.com" className="tm-text-link">TemplateMo</a></p> */}
+									</div>
 								</div>
 							</div>
-							<div className="tm-col-right">
-								<nav className="navbar navbar-expand-lg" id="tm-main-nav">
-									<button className="navbar-toggler toggler-example mr-0 ml-auto" type="button" 
-										data-toggle="collapse" data-target="#navbar-nav" 
-										aria-controls="navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
-										<span><i className="fas fa-bars"></i></span>
-									</button>
-									<div className="collapse navbar-collapse tm-nav" id="navbar-nav">
-										<ul className="navbar-nav text-uppercase">
-											<li className="nav-item active">
-												<a className="nav-link tm-nav-link" href="##">Home <span className="sr-only">(current)</span></a>
-											</li>
-											<li className="nav-item">
-												<a className="nav-link tm-nav-link" href="about.html">About</a>
-											</li>
-											<li className="nav-item">
-												<a className="nav-link tm-nav-link" href="services.html">Services</a>
-											</li>                            
-											<li className="nav-item">
-												<a className="nav-link tm-nav-link" href="contact.html">Contact</a>
-											</li>
-										</ul>                            
-									</div>                        
-								</nav>
-							</div>
-						</div>
-						
-						<div className="tm-row">
-							<div className="tm-col-left"></div>
-							<main className="tm-col-right">
-								<section className="tm-content">
-									<h2 className="mb-5 tm-content-title">Diagoona HTML Template</h2>
-									<p className="mb-5">Diagoona is provided by TemplateMo website. You are allowed to use this template for your websites. You are NOT allowed to redistribute this template ZIP file for a download purpose on any template collection website.</p>
-									<hr className="mb-5"/>
-									<p className="mb-5">Diagoona is Bootstrap v4.4.1 layout. This BG is 50% transparent. You can set the background images auto play settings (true or false in line number 33) in templatemo-script.js file in js folder.</p>                        
-									<Link to="/projects" className="btn btn-primary">Continue...</Link>
-								</section>
-							</main>
+								{/* right box (component) */}
+								<Outlet />
 						</div>
 					</div>
 
 					<div className="tm-row">
 						<ManualBackgroundSelector />
-						<div className="tm-col-right tm-col-footer">
-							<footer className="tm-site-footer text-right">
-								<p className="mb-0">Copyright 2020 Diagoona Co. 
-								
-								| Design: <a rel="nofollow" target="_parent" href="https://templatemo.com" className="tm-text-link">TemplateMo</a></p>
-							</footer>
-						</div>  
+						<Footer />
 					</div>
-
-					{/* <!-- Diagonal background design --> */}
-					{/* <div className="tm-bg">
-						<div className="tm-bg-left"></div>
-						<div className="tm-bg-right"></div>
-					</div> */}
 				</div>
 			</BackgroundSlideshowProvider>
-			
 		</>
 	)
 }
