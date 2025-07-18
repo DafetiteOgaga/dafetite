4614bd0b353e04b9688014c01edae1aaea792aef
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 14:45:49 +0100

    contact.js: removed ipsum text

--- src/newComponent/contact.js
+++ src/newComponent/contact.js
@@ -8,7 +8,10 @@ function Contact () {
 				{/* <!-- contact --> */}
 				<section className="tm-content tm-contact">
 					<h2 className="mb-4 tm-content-title">Contact Us</h2>
-					<p className="mb-85">Etiam et egestas arcu. Fusce congue quis elit vitae commodo. Cras neque mauris, vehicula in ipsum sit amet, faucibus aliquam arcu.</p>
+					{/* <p className="mb-85">
+						Etiam et egestas arcu. Fusce congue quis elit vitae commodo. Cras neque mauris,
+						vehicula in ipsum sit amet, faucibus aliquam arcu.
+					</p> */}
 					<form id="contact-form" action="" method="POST">
 						<div className="form-group mb-4">
 							<input type="text" name="name" className="form-control" placeholder="Name" required="" />
