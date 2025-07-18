83f59210b33f5451b741705e367fe3fe0382664e
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 14:47:28 +0100

    footer.js: revamped the footer to render day, date with suffix and just my name

--- src/newComponent/footer.js
+++ src/newComponent/footer.js
@@ -2,12 +2,12 @@ import React from "react";
 import { DateHook } from "../hooks/DateHook";
 
 function Footer() {
-	const { todayDay, todayDate, todayMonth, todayYear } = DateHook();
-	const formattedDate = `${todayDay}, ${todayMonth} ${todayDate}, ${todayYear}`;
+	const { todayDay, todayDateWithSuffix, todayMonthName, todayYear } = DateHook();
+	const formattedDate = `${todayDay}, ${todayMonthName} ${todayDateWithSuffix}, ${todayYear}`;
 	return (
 		<div className="tm-col-right tm-col-footer">
-			<footer className="tm-site-footer text-right">
-				<p className="mb-0">Copyright &copy; | {formattedDate} | Developed by Dafetite</p>
+			<footer className="tm-site-footer text-right italiced">
+				<p className="mb-0">Copyright &copy; | {formattedDate} | Dafetite</p>
 			</footer>
 		</div>
 	)
