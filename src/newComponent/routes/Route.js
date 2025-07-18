6891b826ee0cbdf30dfa882fa672683f9c2ab9d2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-18 01:06:10 +0100

    Route.js: minor adjustment for dynamic usage

--- src/newComponent/routes/Route.js
+++ src/newComponent/routes/Route.js
@@ -4,17 +4,26 @@ import { Index } from '../index';
 import { Project } from '../project';
 import { Services } from '../services';
 import { Contact } from '../contact';
+import { Home } from '../home';
 
 function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<div />} />
-			<Route path="/new" element={<Index />} />
-			<Route path="/projects" element={<Project />} />
-			<Route path="/services" element={<Services />} />
-			<Route path="/contact" element={<Contact />} />
+			{/* <Route path="new" element={<Index />} />
+			<Route path="projects" element={<Project />} />
+			<Route path="services" element={<Services />} />
+			<Route path="contact" element={<Contact />} /> */}
 			{/* <Route path="/Contact" element={<Contact />} />
 			<Route path="/contact" element={<Contact />} /> */}
+		
+
+			<Route path="new" element={<Index />}>
+				<Route index element={<Home />} />
+				<Route path="projects" element={<Project />} />
+				<Route path="services" element={<Services />} />
+				<Route path="contact" element={<Contact />} />
+			</Route>
 		</Routes>
 	);
 }
