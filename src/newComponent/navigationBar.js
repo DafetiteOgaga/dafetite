887b7103ebc3f7b244d735e15cbfc714f2cee955
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-19 09:21:21 +0100

    navigationBar.js: separated menuitem into component for dynamic rendering

--- src/newComponent/navigationBar.js
+++ src/newComponent/navigationBar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, useLocation } from 'react-router-dom';
+import { useIsMobile } from "../hooks/IsMobile";
 
 const navigation = [
 	{ name: "home", link: "/new" },
@@ -8,50 +9,52 @@ const navigation = [
 	{ name: "contact", link: "/new/contact" }
 ]
 function NavigationBar() {
+	const isMobile = useIsMobile();
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	return (
+		<div className="tm-col-right">
+			<nav className="navbar navbar-expand-lg" id="tm-main-nav">
+				<button
+				onClick={() => setIsMobileMenuOpen(prev=>!prev)}
+				className="navbar-toggler" type="button">
+					<span><i className={`fas ${!isMobileMenuOpen?'fa-bars':'fa-times'}`}></i></span>
+				</button>
+				{!isMobile ?
+					<MenuItems />
+					:
+					isMobileMenuOpen &&
+					<MenuItems />}
+			</nav>
+		</div>
+	)
+}
+
+function MenuItems() {
+	const isMobile = useIsMobile();
+	console.log("Is mobile:", isMobile); // Debugging line to check if it's mobile
 	const pathSegments = useLocation().pathname.split("/");
 	const currentPage = pathSegments.at(-1);
 	// console.log("currentPage path in NavigationBar:", currentPage); // Debugging line to check the currentPage path
 	return (
-		<div className="tm-col-right">
-			<nav className="navbar navbar-expand-lg" id="tm-main-nav">
-				<button className="navbar-toggler toggler-example mr-0 ml-auto" type="button"
-					data-toggle="collapse" data-target="#navbar-nav"
-					aria-controls="navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
-					<span><i className="fas fa-bars"></i></span>
-				</button>
-				<div className="collapse navbar-collapse tm-nav" id="navbar-nav">
-					<ul className="navbar-nav text-uppercase">
-						{navigation.map((item, index) => {
-							// console.log("Navigation item:", item); // Debugging line to check each navigation item
-							const isActive = currentPage === item.name || (currentPage === "new" && item.name === "home");
-							// console.log(`Is "${item.name}" active?`, isActive); // Debugging line to check if the item is active
-							const animetionDelay = `${0.4 + index * 0.4}s`;
-							// console.log("Animation delay for:", item.name, ':', animetionDelay); // Debugging line to check animation delay
-							return (
-								<li key={index}
-								className={`nav-item ${isActive && 'active'} flip-and-fade-in`}
-								style={{ animationDelay: animetionDelay }}>
-									<Link to={item.link}
-									className="nav-link tm-nav-link">{item.name}
-										<span className="sr-only">(current)</span></Link>
-								</li>
-							)
-						})}
-						{/* <li className="nav-item flip-and-fade-in active" style={{ animationDelay: "0.4s" }}>
-							<a className="nav-link tm-nav-link" href="##">Home <span className="sr-only">(current)</span></a>
+		<div className="tm-nav" id="navbar-nav">
+			<ul className="navbar-nav text-uppercase">
+				{navigation.map((item, index) => {
+					// console.log("Navigation item:", item); // Debugging line to check each navigation item
+					const isActive = currentPage === item.name || (currentPage === "new" && item.name === "home");
+					// console.log(`Is "${item.name}" active?`, isActive); // Debugging line to check if the item is active
+					const animetionDelay = `${0.4 + index * 0.4}s`;
+					console.log("Animation delay for:", item.name, ':', animetionDelay); // Debugging line to check animation delay
+					return (
+						<li key={index}
+						className={`nav-item ${isActive && 'active'} ${!isMobile?'flip-and-fade-in':undefined}`}
+						style={{ animationDelay: animetionDelay }}>
+							<Link to={item.link}
+							className="nav-link tm-nav-link">{item.name}
+								<span className="sr-only">(current)</span></Link>
 						</li>
-						<li className="nav-item flip-and-fade-in" style={{ animationDelay: "0.8s" }}>
-							<a className="nav-link tm-nav-link" href="##">About</a>
-						</li>
-						<li className="nav-item flip-and-fade-in" style={{ animationDelay: "1.2s" }}>
-							<a className="nav-link tm-nav-link" href="##">Services</a>
-						</li>
-						<li className="nav-item flip-and-fade-in" style={{ animationDelay: "1.6s" }}>
-							<a className="nav-link tm-nav-link" href="##">Contact</a>
-						</li> */}
-					</ul>
-				</div>
-			</nav>
+					)
+				})}
+			</ul>
 		</div>
 	)
 }
