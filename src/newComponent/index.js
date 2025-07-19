e04ecb622eb2ce8effd2f0599af3bf7a4f68b205
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-19 09:20:43 +0100

    index.js: added resume download and suppport for resume download

--- src/newComponent/index.js
+++ src/newComponent/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 // import '../App.css';
 // import '../global.css';
-import { Outlet } from 'react-router-dom';
-import { BackgroundSlideshow, ManualBackgroundSelector, BackgroundSlideshowProvider } from './hooks/backgroundSlideshow';
+import { Outlet, Link } from 'react-router-dom';
+import { BackgroundSlideshow, ManualBackgroundSelector, BackgroundSlideshowProvider } from '../hooks/backgroundSlideshow';
 import { SiteIconAndSocials } from './siteIconAndSocials';
 import { NavigationBar } from './navigationBar';
 import { Footer } from './footer';
 import dafetite from '../dafetite.png';
+import resume from '../Dafetite_Ogaga.pdf';
 
 const skills = [
 	'Software and Automation Engineer', 'Fullstack (Web and Mobile) Developer', 'IT Support',
 	'Automated Software Tester', 'Programming Tutor', 'ATM and Technical Engineer'
 ]
 function Index () {
-	console.log("Index component loaded");
+	// console.log("Index component loaded");
+	const scrollRef = useRef(null);
+	const [isOverlayed, setIsOverlayed] = useState(false);
 	return (
 		<>
 			<BackgroundSlideshowProvider>
@@ -28,22 +31,36 @@ function Index () {
 							<NavigationBar />
 						</div>
 						</div>
-						<div className="tm-row">
+						<div className="tm-row top-container-mobile">
 							{/* left box */}
 							<div className="tm-col-left">
 								<div className='profile-container'>
 									<img src={dafetite} alt="Dafetite Ogaga" className="dafetite-profile image-fade fade-in-from-top" />
 									<div className="tm-col-left-content">
-										<h1 style={{animationDelay: '0.s', margin: 0, fontSize: 36}}
-										className="tm-sitename text-uppercase flip-and-fade-in">Dafetite O. Ogaga</h1>
+										<div className='flex-display'>
+											<h1 style={{animationDelay: '0.s'}}
+											className="tm-sitename text-uppercase tm-profile-name flip-and-fade-in">
+												Dafetite O. Ogaga
+											</h1>
+											<a
+											href={resume}
+											// download
+											target="_blank"
+											rel="noopener noreferrer"
+											className="btn resume">
+												<span>
+													Get Resume
+												</span>
+											</a>
+										</div>
 										<p className='italiced underlined' style={{marginTop: -10}}>Email: <strong>ogagadafetite@gmail.com</strong></p>
 										{skills.map((skill, index) => {
 											const animetionDelay = `${0.4 + index * 0.4}s`;
 											return (
 												<p key={index}
-												className="tm-slogon skill-fade-in-from-top" style={{
+												className="tm-slogon skill-slogon skill-fade-in-from-top" style={{
 													animationDelay: animetionDelay,
-													lineHeight: '0.3rem',
+													// lineHeight: '1rem',
 												}}>|| {skill}</p>
 											)
 										})}
@@ -54,7 +71,7 @@ function Index () {
 								</div>
 							</div>
 								{/* right box (component) */}
-								<Outlet />
+								<Outlet context={{ scrollRef, isOverlayed, setIsOverlayed }} />
 						</div>
 					</div>
 
