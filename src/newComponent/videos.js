ad419b8bc3bb503e77b0f00e82100e9daba2d570
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-20 13:03:10 +0100

    videos.js: changed 100vw to 100%

--- src/newComponent/videos.js
+++ src/newComponent/videos.js
@@ -115,7 +115,7 @@ const Modal = ({selectedVideo, setSelectedVideo}) => {
 					position: "fixed",
 					top: 0,
 					left: 0,
-					width: "100vw",
+					width: "100%",
 					height: "100vh",
 					backgroundColor: "rgba(0, 0, 0, 0.8)",
 					display: "flex",
