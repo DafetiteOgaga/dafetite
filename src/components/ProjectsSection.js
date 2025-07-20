3a2edc090328e9bf8105d9f04a65322e9332aa15
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-20 13:02:19 +0100

    ProjectsSection.js: changed 100vw to 100%

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -266,7 +266,7 @@ const ProjectsSection = () => {
 				position: "fixed",
 				top: 0,
 				left: 0,
-				width: "100vw",
+				width: "100%",
 				height: "100vh",
 				backgroundColor: "rgba(0, 0, 0, 0.8)",
 				display: "flex",
