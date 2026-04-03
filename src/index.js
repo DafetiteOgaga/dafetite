d9564cf9d5cc2aee82f594b91034c807f6d46eb1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-03 01:24:59 +0100

    index.js: auto detect router setup via platform

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// automatically detect github-pages environment and use HashRouter in that case
+const isGitHubPages = window.location.hostname.includes('github.io');
+const RouterToUse = isGitHubPages ? HashRouter : BrowserRouter;
 root.render(
-  <HashRouter>
+  <RouterToUse>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </RouterToUse>
 );
 
 // If you want to start measuring performance in your app, pass a function
